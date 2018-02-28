9226a4c5dc77c059a89ce05f48f4e752b6562a9f
Simone Bisogni <simone_bisogni@yahoo.it>
2018-02-28 14:23:11 +0100

    Added file_storage setup file

---
+++ file_storage/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='file_storage',
+    version='0.1',
+    description='MinifileBox File Storage',
+    author='Simone Bisogni',
+    install_requirement=[]
+)
+
