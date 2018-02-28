cf18c7d2048733f5d043dd894bf532bb88e033df
Simone Bisogni <simone_bisogni@yahoo.it>
2018-02-28 15:12:29 +0100

    Added extra info to setup.py

--- file_storage/setup.py
+++ file_storage/setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='file_storage',
     version='0.1',
     description='MinifileBox File Storage',
+    url='https://github.com/sbisogni/minifilebox',
     author='Simone Bisogni',
-    install_requirement=[]
+    author_email='simone.bisogni@yahoo.it',
+    python_requires='>=3',
+    install_requirement=[],
+    packagages=find_packages(),
 )
-
