179f65a4d59ca5d6af0a7d90fa235c84172b32aa
Simone Bisogni <simone_bisogni@yahoo.it>
2018-02-28 21:51:44 +0100

    Addded rest endpoint simple documentation

--- components/file_service/app.py
+++ components/file_service/app.py
@@ -13,8 +13,30 @@ FILE_STORAGE = FileStorage.create_test_file_storage()
 @app.route(config.DEFAULT_BASE_URI + '/files/upload', methods=['POST'])
 def upload_file():
     """
-    TODO
-    :return:
+    Upload a file inside the Minifilebox FileStorage.
+
+        curl -X POST http://<host:port>/minifilebox/api/v1/files/upload \
+            -H 'cache-control: no-cache' \
+            -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
+            -F file=@<your_file>
+
+    :return: The file metadata
+        {
+            "chunk_size": <chunk_size>,
+            "chunks": [
+                17,
+                18,
+                19,
+                20,
+                21,
+                22,
+                23,
+                24
+            ],
+            "file_id": <file_unique_id>,
+            "file_name": <file_name>
+        }
+
     """
 
     if 'file' not in request.files:
@@ -34,9 +56,12 @@ def upload_file():
 @app.route(config.DEFAULT_BASE_URI + '/files/download/<int:file_id>', methods=['GET'])
 def download_file(file_id):
     """
+    Download the file with given id
+        curl -X GET http://<host:port>/minifilebox/api/v1/files/download/<file_id> \
+            -H 'cache-control: no-cache' \
 
     :param file_id:
-    :return:
+    :return: The file in mimetype='application/octet-stream'
     """
     try:
         minifile = FILE_STORAGE.load(file_id)
@@ -51,9 +76,27 @@ def download_file(file_id):
 @app.route(config.DEFAULT_BASE_URI + '/files/delete/<int:file_id>', methods=['DELETE'])
 def delete_file(file_id):
     """
+    Delete the file with given id
+        curl -X DELETE http://<host:port>/minifilebox/api/v1/files/delete/<file_id> \
+            -H 'cache-control: no-cache' \
 
-    :param file_id:
-    :return:
+    :param file_id: The file unique id
+    :return: The file metadata of deleted file
+         {
+            "chunk_size": <chunk_size>,
+            "chunks": [
+                17,
+                18,
+                19,
+                20,
+                21,
+                22,
+                23,
+                24
+            ],
+            "file_id": <file_unique_id>,
+            "file_name": <file_name>
+        }
     """
     try:
         minifile = FILE_STORAGE.delete(file_id)
@@ -65,8 +108,43 @@ def delete_file(file_id):
 @app.route(config.DEFAULT_BASE_URI + '/files', methods=['GET'])
 def list_files():
     """
+    List all the files inside the Minifilebox
+        curl -X GET http://<host:port>/minifilebox/api/v1/files
+            -H 'cache-control: no-cache' \
 
-    :return:
+    :return: The list of files metadata
+        [
+            {
+                "chunk_size": 50000000,
+                "chunks": [
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    6,
+                    7,
+                    8
+                ],
+                "file_id": 1,
+                "file_name": "ideaIC-2017.3.4-no-jdk.tar.gz"
+            },
+            {
+                "chunk_size": 50000000,
+                "chunks": [
+                    17,
+                    18,
+                    19,
+                    20,
+                    21,
+                    22,
+                    23,
+                    24
+                ],
+                "file_id": 3,
+                "file_name": "ideaIC-2017.3.4-no-jdk.tar.gz"
+            }
+        ]
     """
     minifile_list = FILE_STORAGE.list()
     return jsonify([m.to_dict() for m in minifile_list]), 200
