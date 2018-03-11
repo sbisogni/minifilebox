0c97cc134a05f1ad86757303dbe4f9404cf33ebb
Simone Bisogni <simone_bisogni@yahoo.it>
2018-03-11 21:28:42 +0100

    Support for remote ContextStore

---
+++ components/context_service/app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from flask import Flask, request, jsonify
+from file_storage.MemoryStorage import ContextStoreInMemory, Minifile
+import config
+
+app = Flask(__name__)
+
+context_store = ContextStoreInMemory()
+
+
+@app.route(config.MINIFILEBOX_BASE_URI + '/context', methods=['POST'])
+def store_context():
+    file = Minifile().from_dict(request.json)
+    context_store.save(file)
+    return jsonify(status='OK')
+
+
+@app.route(config.MINIFILEBOX_BASE_URI + '/context/<string:file_id>', methods=['GET'])
+def load_context(file_id):
+    return jsonify(context_store.load(file_id).to_dict())
+
+
+@app.route(config.MINIFILEBOX_BASE_URI + '/context/<string:file_id>', methods=['DELETE'])
+def delete_context(file_id):
+    context_store.delete(file_id)
+    return jsonify(status='OK')
+
+
+@app.route(config.MINIFILEBOX_BASE_URI + '/context', methods=['GET'])
+def list_context():
+    print("Context Context: %s" % context_store.list())
+    return jsonify([c.to_dict() for c in context_store.list()])
+
+
+if __name__ == '__main__':
+    # Overriding default HTML exception handler
+    # for ex in default_exceptions:
+    #     app.register_error_handler(ex, handle_error)
+    app.run(host='0.0.0.0', port=5002)
